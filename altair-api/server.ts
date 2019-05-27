529faf2f84f3b85293e9781ee09a1e88fc9cc7b1
Guillermo Lopez <guillermolopez2529@gmail.com>
2019-05-27 18:33:53 -0400

    feat: add utils project

--- altair-api/server.ts
+++ altair-api/server.ts
@@ -1,11 +1,11 @@
 import http from 'http';
 import express from 'express';
-import altairConfig from '@xarples/altair-config'
+import altairConfig from '@xarples/altair-config';
 
 const { api } = altairConfig;
-const app = express()
-const server = http.createServer(app)
+const app = express();
+const server = http.createServer(app);
 
 server.listen(api.port, () => {
-  console.log(`Server listening on http://localhost:${api.port}`)
-})
+  console.log(`Server listening on http://localhost:${api.port}`);
+});
